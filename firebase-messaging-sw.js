1e560ec287d4f5b382b88235571bc60991285c89
costmarsoffers-wh <costmarsoffers@gmail.com>
2025-12-20 00:30:58 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,25 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyBwMv6b3Pd6Xjn5Nu4Q4RFV_XFR_6BSsoM",
+  authDomain: "na3ma-customers.firebaseapp.com",
+  databaseURL: "https://na3ma-customers-default-rtdb.firebaseio.com",
   projectId: "na3ma-customers",
+  storageBucket: "na3ma-customers.firebasestorage.app",
   messagingSenderId: "118299493181",
   appId: "1:118299493181:web:ea8a0b2f1bfbc2bf0d7b54"
 });
 
-firebase.messaging();
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/Costmars-offers-/icon.png"
+    }
+  );
+});
+
